c3670ae3213b24dc6abc30f96164f2c6865de1f9
neilthemippy <shapironeila@gmail.com>
2023-06-21 13:35:43 -0500

    substrings

--- main.rb
+++ main.rb
@@ -1,6 +1,41 @@
-"Welcome" + "to" + "Ruby"
+# single line comment
 
-"Welcome" << "to" << "Ruby!"
+=begin
+multi-line comment
+=end
 
-"Welcome".concat("to").concat("Ruby!")
 
+# Addition
+1 + 1   #=> 2
+
+# Subtraction
+2 - 1   #=> 1
+
+# Multiplication
+2 * 2   #=> 4
+
+# Division
+10 / 5  #=> 2
+
+# Exponent
+2 ** 2  #=> 4
+3 ** 4  #=> 81
+
+# Modulus (find the remainder of division)
+8 % 2   #=> 0  (8 / 2 = 4; no remainder)
+10 % 4  #=> 2  (10 / 4 = 2 with a remainder of 2)
+
+"Welcome" + "to" + "Ruby" # concatenate
+
+"Welcome" << "to" << "Ruby!" #concatenate
+
+"Welcome".concat("to").concat("Ruby!") #concatanate
+
+# substrings how to access specific characters in a string
+"hello"[0]
+
+"hello"[0..1]
+
+"hello"[0, 4]
+
+"hello"[-1]
